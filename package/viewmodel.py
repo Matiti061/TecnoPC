616cf71005f4829b5292aa3363a56c693f6cdc96
Esteban Urrutia <esteban125333@gmail.com>
2025-04-25 01:14:01 -0400

    refactor(pylint): enable R0903 on viewmodel

--- package/viewmodel.py
+++ package/viewmodel.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0114,C0115,R0903
+# pylint: disable=C0114,C0115,C0116
 # For documentation please check the Model itself, not the ViewModel.
 
 from .model import Model
@@ -7,7 +7,23 @@ from .model import Model
 class ViewModel:
     def __init__(self, model: Model):
         self._model = model
-        self.manager = self._model.manager
-        self.store = self._model.store
-        self.worker = self._model.worker
-        self.product = self._model.product
+        self._manager = self._model.manager
+        self._store = self._model.store
+        self._worker = self._model.worker
+        self._product = self._model.product
+
+    @property
+    def manager(self):
+        return self._manager
+
+    @property
+    def store(self):
+        return self._store
+
+    @property
+    def worker(self):
+        return self._worker
+
+    @property
+    def product(self):
+        return self._product
