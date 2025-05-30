a965af9e58717f33c8a33145b58a287dddfebde9
Esteban Urrutia <esteban125333@gmail.com>
2025-05-30 16:10:04 -0400

    fix(package): Modify __init__.py

---
+++ package/__init__.py
@@ -0,0 +1,24 @@
+from .model import (
+    _InternalModel,
+    Employee,
+    EmployeeModel,
+    Manager,
+    ManagerModel,
+    Model,
+    Product,
+    ProductModel,
+    Store,
+    StoreModel
+)
+from .view import (
+    BaseWidget,
+    CustomDialog,
+    EmployeeWidget,
+    FormAddProduct,
+    LoginWidget,
+    ManagementWidget,
+    ModifyEmployeeWidget,
+    ModifyProductWidget,
+    View
+)
+from .viewmodel import ViewModel
