c3e01234b8e265082ae58a955c80ab37bb289f68
Esteban Urrutia <esteban125333@gmail.com>
2025-06-01 23:56:47 -0400

    feat(model): implement CRUD base

--- package/model/model.py
+++ package/model/model.py
@@ -1,16 +1,12 @@
 from .internal_model import InternalModel
-from .manager_model import ManagerModel
-from .store_model import StoreModel
-from .employee_model import EmployeeModel
-from .product_model import ProductModel
-from .sale_model import SaleModel
+from .crud import CRUD
 
 
 class Model:
     def __init__(self):
         self.model = InternalModel()
-        self.manager = ManagerModel(self.model)
-        self.store = StoreModel(self.model)
-        self.employee = EmployeeModel(self.model)
-        self.product = ProductModel(self.model)
-        self.sale = SaleModel(self.model)
+        self.manager = CRUD(self.model, "managers")
+        self.store = CRUD(self.model, "stores")
+        self.employee = CRUD(self.model, "employees", "stores")
+        self.product = CRUD(self.model, "products", "stores")
+        self.sale = CRUD(self.model, "sales")
