67c995162912b6e983c209b16405c24ff95622ff
Esteban Urrutia <esteuwu@proton.me>
2025-06-19 22:13:56 -0400

    Model ok

--- package/model/internal_model.py
+++ package/model/internal_model.py
@@ -3,42 +3,26 @@ import json
 
 class InternalModel:
     def __init__(self):
+        self.keys = ["clients", "discounts", "employees", "managers", "products", "providers", "sales", "stores"]
         try:
             with open("data.json", encoding="utf-8") as file:
-                self.data: dict = json.load(file)
+                self.json: dict = json.load(file)
         except FileNotFoundError:
-            self.data = json.loads('{"managers": [], "stores": [], "sales": [], "client": [], "providers": [], "discount": []}')
+            self.json = json.loads("{}")
+            for key in self.keys:
+                self.json[key] = []
             self.save()
-        except json.decoder.JSONDecodeError as e:
-            raise RuntimeError(f"JSON decoding error, manual intervention needed: {e}") from e
+        except json.decoder.JSONDecodeError as error:
+            raise error
 
-    def edit_entity(self, key: str, entity_uuid: str, payload: dict[str, int | str]):
-        index = self.locate_entity(key, entity_uuid)
-        self.data[key][index].update(payload)
-        self.save()
-
-    def delete_entity(self, key: str, entity_uuid: str):
-        index = self.locate_entity(key, entity_uuid)
-        del self.data[key][index]
-        self.save()
-
-    def locate_entity(self, key: str, entity_uuid: str):
-        if key not in ["managers", "stores", "employees", "products", "sales", "client", "providers"]:
+    def index_of(self, key: str, item_uuid: str):
+        if key not in self.keys:
             raise ValueError("Invalid key")
-        for index, value in enumerate(self.data[key]):  # type: int, dict
-            if value["uuid"] == entity_uuid:
+        for index, value in enumerate(self.json[key]):  # type: int, dict
+            if value["uuid"] == item_uuid:
                 return index
-        raise ValueError("Entity not found")
-
-    def locate_nested_entity(self, keys: list[str], entity_uuids: list[str]):
-        if keys[1] not in ["employees", "products"]:
-            raise ValueError("Invalid nested key")
-        i = self.locate_entity(keys[0], entity_uuids[0])
-        for j, value in enumerate(self.data[keys[0]][i][keys[1]]):  # type: int, dict
-            if value["uuid"] == entity_uuids[1]:
-                return i, j
-        raise ValueError("Nested entity not found")
+        raise ValueError("No such item")
 
     def save(self):
         with open("data.json", "w", encoding="utf-8") as file:
-            json.dump(self.data, file, indent=4)
+            json.dump(self.json, file, indent=4)
--- package/model/model.py
+++ package/model/model.py
@@ -1,21 +1,16 @@
+# pylint: disable=R0902
+from .crud import CRUD
 from .internal_model import InternalModel
-from .manager_model import ManagerModel
-from .store_model import StoreModel
-from .employee_model import EmployeeModel
-from .client_model import ClientModel
-from .product_model import ProductModel
-from .sale_model import SaleModel
-from .provider_model import ProviderModel
 
 
 class Model:
     def __init__(self):
         self.model = InternalModel()
-        self.manager = CRUD(self.model, "managers")
-        self.store = CRUD(self.model, "stores")
-        self.employee = CRUD(self.model, "employees", "stores")
-        self.client = ClientModel(self.model)
-        self.product = CRUD(self.model, "products", "stores")
-        self.sale = CRUD(self.model, "sales")
-        self.provider = ProviderModel(self.model)
-        self.discount = DiscountModel(self.model)
+        self.clients = CRUD(self.model, "clients")
+        self.discounts = CRUD(self.model, "discounts")
+        self.employees = CRUD(self.model, "employees")
+        self.managers = CRUD(self.model, "managers")
+        self.products = CRUD(self.model, "products")
+        self.providers = CRUD(self.model, "providers")
+        self.sales = CRUD(self.model, "sales")
+        self.stores = CRUD(self.model, "stores")
